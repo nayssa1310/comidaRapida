fcb54b80e7a39446a87dc10f4cbf8137a058a0f6
nayssa1310 <nayssa1310@gmail.com>
2025-11-23 10:52:56 -0500

    correccion Header.txt

--- comida/src/componentes/Header.tsx
+++ comida/src/componentes/Header.tsx
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import '../App.css'
+
 interface HeaderProps {
   mesa: string;
-  abrirLogin: () => void; // ← CORRECTO
+  abrirLogin: () => void;
 }
 
 export default function Header({ mesa, abrirLogin }: HeaderProps) {
@@ -9,13 +12,11 @@ export default function Header({ mesa, abrirLogin }: HeaderProps) {
     <header className="header-sticky p-3 mb-4 shadow">
       <div className="container d-flex justify-content-between align-items-center">
 
-        {/* LOGO + NOMBRE */}
         <div className="d-flex align-items-center gap-2">
           <img src="/logo.png" alt="Logo" width={50} />
           <h1 className="text-pink-700 fw-bold">Delicias Rápidas</h1>
         </div>
 
-        {/* NAVEGACIÓN */}
         <div className="d-flex gap-3 align-items-center">
           <span className="fw-bold">Mesa: {mesa}</span>
           <nav className="d-flex gap-3">
@@ -24,14 +25,13 @@ export default function Header({ mesa, abrirLogin }: HeaderProps) {
             <a href="#" className="text-decoration-none text-pink-700 fw-semibold">Contacto</a>
           </nav>
         </div>
-
-        {/* ICONO LOGIN */}
+        
         <div className="iconos-header">
-          <i
-            className="fa-solid fa-user fs-4"
-            style={{ cursor: "pointer", color: "#d63384" }}
-            onClick={abrirLogin}  
-          ></i>
+          <FontAwesomeIcon
+            icon={faUser}
+            style={{ cursor: "pointer", color: "#d63384", fontSize: "1.25rem" }}
+            onClick={abrirLogin}
+          />
         </div>
 
       </div>
